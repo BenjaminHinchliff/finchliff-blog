4ea8680c7c8d3e93e5891e51dec90bbe4521480a
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-09-21 17:41:31 -0700

    add apollo provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import '../styles/globals.css';
+import type {AppProps} from 'next/app';
+import {ApolloClient, ApolloProvider, HttpLink, InMemoryCache} from '@apollo/client';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const link = new HttpLink({
+	uri: '/api/projects',
+});
+
+const client = new ApolloClient({
+	link,
+	cache: new InMemoryCache(),
+});
+
+function MyApp({Component, pageProps}: AppProps) {
+	return <ApolloProvider client={client}>
+		<Component {...pageProps} />
+	</ApolloProvider>;
 }
-export default MyApp
+
+export default MyApp;
