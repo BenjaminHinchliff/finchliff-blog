98b84110df519bb523cea0676f07d244972508c4
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-09-22 22:11:40 -0700

    update header styles

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Image from 'next/image';
+import logoPic from '../public/logo.svg';
 
 export default function Header() {
 	return (
-		<h1 className="text-center text-6xl font-light py-16 rounded shadow-lg bg-yellow-600">
-			Benjamin Hinchliff
-		</h1>
+		<div className="text-center text-6xl font-light py-20 rounded bg-yellow-600 overflow-hidden">
+			<div className="absolute left-0 right-0 top-0 z-0 opacity-50">
+				<Image src={logoPic} alt="logo picture"></Image>
+			</div>
+			<h1 className="relative z-10">Benjamin Hinchliff</h1>
+		</div>
 	);
 }
