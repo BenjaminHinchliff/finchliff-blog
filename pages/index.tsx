89bf6a45d7d4e8b139248e8a7722cf63585b8d50
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-09-22 19:38:59 -0700

    index tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import Projects from '../components/projects';
 
 const Home: NextPage = () => (
 	<div className="container mx-auto">
-		<h1 className="text-center text-4xl font-light my-4">
+		<h1 className="text-center text-6xl font-light py-16 rounded shadow-lg bg-yellow-600">
 			Benjamin Hinchliff
 		</h1>
 		<Projects />
