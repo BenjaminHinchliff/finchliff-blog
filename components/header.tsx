daa67e66396b1aee54f88ca83dd43c7d063e4506
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-09-22 19:48:55 -0700

    refactor header

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+	return (
+		<h1 className="text-center text-6xl font-light py-16 rounded shadow-lg bg-yellow-600">
+			Benjamin Hinchliff
+		</h1>
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
 import type {NextPage} from 'next';
 import React from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import Header from '../components/header';
 import Projects from '../components/projects';
 
 const Home: NextPage = () => (
 	<div className="container mx-auto">
-		<h1 className="text-center text-6xl font-light py-16 rounded shadow-lg bg-yellow-600">
-			Benjamin Hinchliff
-		</h1>
+		<Header />
 		<Projects />
 	</div>
 );
