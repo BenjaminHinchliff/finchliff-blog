6106f19da62c888ed44a7c7922b5748dec31b420
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-09-23 08:48:42 -0700

    add tags for favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/globals.css';
 import type {AppProps} from 'next/app';
+import Head from 'next/head';
 import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 import {relayStylePagination} from '@apollo/client/utilities';
 
@@ -21,9 +22,38 @@ const client = new ApolloClient({
 
 function MyApp({Component, pageProps}: AppProps) {
 	return (
-		<ApolloProvider client={client}>
-			<Component {...pageProps} />
-		</ApolloProvider>
+		<>
+			<Head>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+				<link
+					rel="mask-icon"
+					href="/safari-pinned-tab.svg"
+					color="#000000"
+				/>
+				<meta name="msapplication-TileColor" content="#ffffff" />
+				<meta name="theme-color" content="#ffffff" />
+			</Head>
+			<ApolloProvider client={client}>
+				<Component {...pageProps} />
+			</ApolloProvider>
+		</>
 	);
 }
 
