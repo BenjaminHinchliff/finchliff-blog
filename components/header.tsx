7ad51f9e77aec2f6837a00343a3e4fbc941cee7a
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-09-23 21:15:02 -0700

    tweak opacity

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ import logoPic from '../public/logo.svg';
 export default function Header() {
 	return (
 		<div className="relative text-center text-6xl font-light py-20 rounded bg-yellow-600 shadow overflow-hidden">
-			<div className="absolute left-0 right-0 top-0 z-0 opacity-50">
+			<div className="absolute left-0 right-0 top-0 z-0 opacity-30">
 				<Image src={logoPic} alt="logo picture"></Image>
 			</div>
 			<h1 className="relative z-10">Benjamin Hinchliff</h1>
