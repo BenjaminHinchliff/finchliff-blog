4edf417751dfa63d68240d73409a4616a08a5bd4
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-09-23 17:19:34 -0700

    tweak header styles

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import logoPic from '../public/logo.svg';
 
 export default function Header() {
 	return (
-		<div className="text-center text-6xl font-light py-20 rounded bg-yellow-600 overflow-hidden">
+		<div className="relative text-center text-6xl font-light py-20 rounded bg-yellow-600 shadow overflow-hidden">
 			<div className="absolute left-0 right-0 top-0 z-0 opacity-50">
 				<Image src={logoPic} alt="logo picture"></Image>
 			</div>
