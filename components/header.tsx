d7af67d2f626605bb04bc6b09ff89dd00bebcc58
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-09-27 21:29:17 -0700

    add placeholder navbar and icons

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,17 @@
 import type {FunctionComponent} from 'react';
 import Image from 'next/image';
 import logoPic from '../public/logo.svg';
+import Navbar from './navbar';
 
 const Header: FunctionComponent = () => (
-	<div className="relative text-center text-6xl font-light py-20 rounded bg-yellow-600 shadow overflow-hidden">
-		<div className="absolute left-0 right-0 top-0 z-0 opacity-30">
-			<Image src={logoPic} alt="logo picture"></Image>
+	<div>
+		<div className="relative text-center py-20 rounded bg-yellow-600 shadow overflow-hidden">
+			<div className="absolute left-0 right-0 top-0 z-0 opacity-30">
+				<Image src={logoPic} alt="logo picture"></Image>
+			</div>
+			<h1 className="relative text-6xl font-light">Benjamin Hinchliff</h1>
 		</div>
-		<h1 className="relative z-10">Benjamin Hinchliff</h1>
+		<Navbar />
 	</div>
 );
 
