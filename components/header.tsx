7bff877649aefd043e6b7b7adfe0a5ef102bc128
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2022-01-19 23:12:23 -0800

    update packages

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import logoPic from '../public/logo.svg';
 
 const Header: FunctionComponent = () => (
 	<div>
-		<div className="relative text-center py-20 rounded bg-yellow-600 shadow overflow-hidden">
+		<div className="relative text-center py-20 rounded bg-orange-500 shadow overflow-hidden">
 			<div className="absolute left-0 right-0 top-0 z-0 opacity-30">
 				<Image src={logoPic} alt="logo picture"></Image>
 			</div>
